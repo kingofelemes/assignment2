77e7bf02bd455e8f02fa0c9767861d929c11bcc9
kingofelemes <123185720+kingofelemes@users.noreply.github.com>
2023-10-03 15:39:07 +0600

    Add files via upload

---
+++ в/js/lang.js
@@ -0,0 +1,32 @@
+const langArr = {
+    "unit" :  {
+        "ru": "Madi Tazhibayev",
+        "en": "Madi Tazhibayev",
+        "kz": "Madi Tazhibayev",
+    }, 
+    "chip": {
+        "ru": "графический чип",
+        "en": "graphics processing unit",
+        "ua": "графічний чіп",
+    }, 
+    "memory": {
+        "ru": "объем памяти",
+        "en": "memory size",
+        "ua": "oбсяг пам'яті",
+    }, 
+    "memory-type": {
+        "ru": "тип памяти",
+        "en": "memory type",
+        "ua": "nип пам'яті",
+    }, 
+    "cool": {
+        "ru": "тип системы охлаждения",
+        "en": "сooling system type",
+        "ua": "тип системи охолодження",
+    }, 
+    "more": {
+        "ru": "детально о товаре",
+        "en": "more details",
+        "ua": "детально про товар",
+    }, 
+}
